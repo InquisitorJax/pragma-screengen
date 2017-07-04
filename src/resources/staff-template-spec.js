8199167f36c72f1f07068f8cebce64ccf2a3324f
Malcolm Jack <inquisitorjax@gmail.com>
2017-07-04 11:58:33 +0200

    project 0.2 updates

--- src/resources/staff-template-spec.js
+++ src/resources/staff-template-spec.js
@@ -184,7 +184,7 @@ export const staffTemplateSpec =
                             element: "input",
                             "title": "Site",
                             "field": "siteCode",
-                            "description": "siteDescription"
+                            "description": "${model.siteDescription}"
                         }]
                 },
                 {
